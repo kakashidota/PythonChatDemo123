aebe3777ed0263b02062ceaf8525b565b4e500d6
Kakashidota <robin.kamo@live.se>
2025-04-16 18:12:43 +0200

    added gui

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 import tkinter as tk
 import pymongo
 
+#Connect to MongoDb
+client = pymongo.MongoClient("mongodb://localhost:27017")
+db = client["PythonChat"]
+messages_col = db["messages"]
+
+
+# GUI
+root = tk.Tk()
+root.title("PythonChat")
+root.geometry("300x300")
+
+
+entry = tk.Entry(root, width=40)
+entry.pack(pady=5)
+
+
+send_button = tk.Button(root, text="Send")
+send_button.pack(pady=5)
+
+
+root.mainloop()
+
+
+
+
+
 
 # git status (frivilligt)
 # git add .
