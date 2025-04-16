1bb2db8ed4de7920bbddb2e171b2a91f27c923fd
Kakashidota <robin.kamo@live.se>
2025-04-16 18:49:14 +0200

    finished product

--- app.py
+++ app.py
@@ -26,6 +26,16 @@ send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack(pady=5)
 
 
+message_label = tk.Label(root, text="Messages:", justify="left")
+message_label.pack()
+
+def fetch_messages():
+    messages = messages_col.find().sort("_id")
+    message_label.config(text="Messages:\n " + "\n".join(f"- {m['text']}" for m in messages))
+    root.after(2000, fetch_messages)
+
+
+fetch_messages()
 root.mainloop()
 
 
